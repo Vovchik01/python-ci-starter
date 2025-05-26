77f00f7b0c328bee5d3ed93222b60cfcab76187d
vovik <vovik@mmm.su>
2025-05-26 11:37:00 +0300

    added multiply and wrong test

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,13 @@
-from calculator.operations import add, subtract
+from calculator.operations import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
 
+def test_add():
+    assert add(3, 3) == 7
+
 def test_subtract():
     assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(4, 6) == 24
